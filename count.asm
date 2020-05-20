5290b2c21bf4e4eeccdfc281359e5ee872b109ac
Artyom Danilov <defytheflow@gmail.com>
2020-05-21 01:45:25 +0300

    Remove unnecessaty directories, improve count program

--- count.asm
+++ count.asm
@@ -1,27 +1,55 @@
 ;
-; Prints a number on each line up to COUNT.
+; Description:
+;   Prints a number on each line up to <limit>.
+;
+; Usage:
+;  count <limit>
 ;
 
 %include 'stdlib.asm'
 
-%define COUNT 100
+section .data
+    usage_msg  db  'Usage: count <limit>.', 0
 
 section .text
     global _start
 
 _start:
+    pop  rcx         ; pop number of arguments.
+    pop  rdx         ; pop program name.
+    dec  rcx         ; decrease number of args.
+
+; Check required argument.
+    cmp  rcx, 1
+    jne  .usage
     mov  rcx, 0
 
-.loop:
-    inc  rcx
+; Convert limit to int.
+    pop  rbx         ; pop limit.
+    mov  rdi, rbx
+    call stoi
+    mov  rbx, rax
 
-    mov  rdi, rcx
+    xor  rcx, rcx    ; clear counter.
+
+.count_loop:
+    cmp  rcx, rbx    ; have we reached the limit?
+    je   .finish
+
     push rcx
+    mov  rdi, rcx
     call intprintln
     pop  rcx
 
-    cmp  rcx, COUNT
-    jne  .loop
+    inc  rcx
+    jmp  .count_loop
 
-    mov  rdi, 0
+.usage:
+    mov  rdi, usage_msg
+    call strprintln
+    mov  rdi, 1
+    call exit
+
+.finish:
+    mov rdi, 0
     call exit
--- helloworld.asm
+++ helloworld.asm
@@ -1,17 +1,17 @@
 ;
-; Writes 'Hello world' to stdout.
+; Prints 'Hello world' to stdout.
 ;
 
 %include 'stdlib.asm'
 
 section .data
-    msg db "Hello, World", 0
+    msg db 'Hello, World', 0
 
 section .text
     global _start
 
 _start:
-    mov rdi, msg
+    mov  rdi, msg
     call strprintln
-    mov rdi, 0
+    mov  rdi, 0
     call exit
